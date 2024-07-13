183affb8b24aefef8d2419c57e9d9136f071775f
İsmail Dinger <ismaiildinger@gmail.com>
2024-07-13 12:55:47 +0300

    navigation bottom bar widget eklendi

--- lib/widgets/bottom_navigation_bar.dart
+++ lib/widgets/bottom_navigation_bar.dart
@@ -1,32 +1,75 @@
-
 import 'package:flutter/material.dart';
 
-class CustomBottomNavigationBar extends StatelessWidget {
+class CustomBottomNavigationBar extends StatefulWidget {
   const CustomBottomNavigationBar({Key? key}) : super(key: key);
 
   @override
+  _CustomBottomNavigationBarState createState() => _CustomBottomNavigationBarState();
+}
+
+class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Anasayfa"),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.medical_services),
-          label: 'Ecza Kutusu',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.camera_alt),
-          label: 'Kamera',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.notifications),
-          label: 'İlaç Alarmı',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.account_circle),
-          label: 'Profilim',
-        ),
-      ],
+    return Container(
+      height: 74,
+      child: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: _buildIcon(Icons.home_outlined, 0),
+            label: 'Anasayfa',
+          ),
+          BottomNavigationBarItem(
+            icon: _buildIcon(Icons.medical_services_outlined, 1),
+            label: 'Ecza Kutusu',
+          ),
+          BottomNavigationBarItem(
+            icon: _buildIcon(Icons.camera_alt, 2),
+            label: 'Kamera',
+            backgroundColor: Colors.red,
+           
+
+          ),
+          BottomNavigationBarItem(
+            icon: _buildIcon(Icons.notifications_outlined, 3),
+            label: 'İlaç Alarmı',
+          ),
+          BottomNavigationBarItem(
+            icon: _buildIcon(Icons.person_outline, 4),
+            label: 'Profilim',
+          ),
+        ],
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
+        backgroundColor: const Color(0xFFD5E7F2),
+        iconSize: 24, // İkonların boyutunu ayarlıyoruz
+        selectedLabelStyle: const TextStyle(fontSize: 12, fontWeight: FontWeight.bold), // Seçili öğenin yazı boyutu
+        unselectedLabelStyle: const TextStyle(fontSize: 12, fontWeight: FontWeight.bold), // Seçili olmayan öğelerin yazı boyutu
+      ),
+    );
+  }
+
+  Widget _buildIcon(IconData iconData, int index) {
+    final isSelected = _selectedIndex == index;
+    return Container(
+      decoration: isSelected
+          ? BoxDecoration(
+              color: const Color(0xFF93ABBF), // Seçili öğe arka plan rengi
+              borderRadius: BorderRadius.circular(20),
+            )
+          : null,
+      padding: const EdgeInsets.all(8),
+      child: Icon(iconData),
     );
   }
 }
