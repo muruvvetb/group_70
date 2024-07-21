042300750e1226c9d9bf9c7e9e44cc1689fcc2b8
mfiratf <mfirat.819@gmail.com>
2024-07-21 21:55:51 +0300

    Added constructor to home_page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
+  const HomePage({super.key});
   @override
   _HomePageState createState() => _HomePageState();
 }
