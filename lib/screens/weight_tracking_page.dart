23ec334445fd10767e6823895c5893669cbb2929
İsmail Dinger <ismaiildinger@gmail.com>
2024-08-02 22:32:42 +0300

    weight tracking widget created

--- lib/screens/weight_tracking_page.dart
+++ lib/screens/weight_tracking_page.dart
@@ -1,30 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:cep_eczane/screens/yakindaki_eczaneler.dart';
-import 'search_screen.dart';
-import 'weight_tracking_page.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class WeightTrackingPage extends StatefulWidget {
+  final Function onWeightEntryAdded;
+
+  const WeightTrackingPage({super.key, required this.onWeightEntryAdded});
 
   @override
-  _HomePageState createState() => _HomePageState();
+  _WeightTrackingPageState createState() => _WeightTrackingPageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  List<Map<String, dynamic>> _weightEntries = [];
+class _WeightTrackingPageState extends State<WeightTrackingPage> {
+  final List<Map<String, dynamic>> _weightEntries = [];
+  final TextEditingController _weightController = TextEditingController();
+  String? userId;
 
   @override
   void initState() {
     super.initState();
+    _initializeUserId();
     _fetchWeightEntries();
   }
 
+  Future<void> _initializeUserId() async {
+    User? user = FirebaseAuth.instance.currentUser;
+    setState(() {
+      userId = user?.uid;
+    });
+  }
+
   Future<void> _fetchWeightEntries() async {
-    final userId = FirebaseAuth.instance.currentUser?.uid;
     if (userId != null) {
       final QuerySnapshot snapshot = await FirebaseFirestore.instance
           .collection('weight_entries')
@@ -39,9 +47,40 @@ class _HomePageState extends State<HomePage> {
               })
           .toList();
       setState(() {
-        _weightEntries = fetchedEntries;
+        _weightEntries.addAll(fetchedEntries);
       });
     }
+  }
+
+  Future<void> _saveWeightEntryToFirestore(String weight) async {
+    if (userId != null) {
+      final DateTime now = DateTime.now();
+      final Map<String, dynamic> entry = {
+        'date': Timestamp.fromDate(now),
+        'weight': double.parse(weight),
+      };
+
+      await FirebaseFirestore.instance
+          .collection('weight_entries')
+          .doc(userId)
+          .collection('entries')
+          .add(entry);
+    }
+  }
+
+  void _addWeightEntry(String weight) async {
+    await _saveWeightEntryToFirestore(weight);
+
+    setState(() {
+      _weightEntries.add({
+        'date': DateTime.now(),
+        'weight': double.parse(weight),
+      });
+      _weightEntries.sort((a, b) => a['date'].compareTo(b['date']));
+    });
+
+    widget.onWeightEntryAdded();
+    _weightController.clear();
   }
 
   List<FlSpot> _generateSpots() {
@@ -58,113 +97,49 @@ class _HomePageState extends State<HomePage> {
         .toList();
   }
 
-  void _onWeightEntryAdded() {
-    setState(() {
-      _fetchWeightEntries();
-    });
+  List<String> _generateDates() {
+    if (_weightEntries.isEmpty) {
+      return ['Boş'];
+    }
+    return _weightEntries.map((entry) {
+      return DateFormat('dd MMM').format(entry['date'] as DateTime);
+    }).toList();
+  }
+
+  double getCurrentWeight() {
+    if (_weightEntries.isNotEmpty) {
+      return _weightEntries.last['weight'];
+    }
+    return 0.0;
+  }
+
+  double getProgress() {
+    if (_weightEntries.length > 1) {
+      return _weightEntries.last['weight'] - _weightEntries.first['weight'];
+    }
+    return 0.0;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Anasayfa'),
+        title: const Text('Kilo Takibi'),
         centerTitle: true,
-        actions: [
-          
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const SearchScreen(),
-                ),
-              );
-            },
-          ),
-        ],
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
-                elevation: MaterialStateProperty.all<double>(0),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                padding: MaterialStateProperty.all<EdgeInsets>(
-                  const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-                ),
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => YakindakiEczaneler(),
-                  ),
-                );
-              },
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  const Text("Yakındaki Eczaneler",
-                      style: TextStyle(fontSize: 20, color: Colors.black)),
-                  Container(
-                    width: double.infinity,
-                    height: 300,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      image: const DecorationImage(
-                        image: AssetImage('icons/map.png'),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          GestureDetector(
-            onTap: () async {
-              await Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => WeightTrackingPage(
-                    onWeightEntryAdded: _onWeightEntryAdded,
-                  ),
-                ),
-              );
-              _onWeightEntryAdded(); // Bu satır eklendi
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Container(
-                width: double.infinity,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
                 height: 250,
-                padding: const EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).scaffoldBackgroundColor, // Ana ekranın arka plan rengiyle aynı
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 2,
-                      blurRadius: 5,
-                      offset: const Offset(0, 3),
-                    ),
-                  ],
-                ),
                 child: LineChart(
                   LineChartData(
                     backgroundColor: Colors.transparent,
                     gridData: FlGridData(
-                      show: true, // Gridleri göster
+                      show: true,
                       horizontalInterval: 1,
                       verticalInterval: 1,
                       getDrawingHorizontalLine: (value) {
@@ -231,7 +206,8 @@ class _HomePageState extends State<HomePage> {
                       LineChartBarData(
                         spots: _generateSpots(),
                         isCurved: true,
-                        color: Color.fromARGB(255, 148, 189, 222),
+                          color: Color.fromARGB(255, 148, 189, 222),
+
                         barWidth: 4,
                         dotData: FlDotData(show: true),
                         belowBarData: BarAreaData(
@@ -250,16 +226,80 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ),
               ),
-            ),
+              const SizedBox(height: 20),
+              Card(
+                elevation: 4,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "${getCurrentWeight().toStringAsFixed(1)} kg",
+                        style: const TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        "Güncel Kilo",
+                        style: TextStyle(fontSize: 18, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Card(
+                elevation: 4,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "${getProgress().toStringAsFixed(1)} kg",
+                        style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: getProgress() < 0 ? Colors.red : Colors.green,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        "İlerleme",
+                        style: TextStyle(fontSize: 18, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _weightController,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        hintText: "Kilonuzu girin (kg)",
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.add),
+                    onPressed: () {
+                      if (_weightController.text.isNotEmpty) {
+                        _addWeightEntry(_weightController.text);
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
-}
-
-void main() {
-  runApp(const MaterialApp(
-    home: HomePage(),
-  ));
 }
